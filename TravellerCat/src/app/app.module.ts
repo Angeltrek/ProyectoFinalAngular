7a14f6abea3c25e8167b9f06c109ddd5fb9ad503
Angeltrek <angeltrek007@gmail.com>
2021-11-25 13:25:14 -0600

    Actualizando pagina

--- TravellerCat/src/app/app.module.ts
+++ TravellerCat/src/app/app.module.ts
@@ -4,21 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-<<<<<<< HEAD
 import { FooterComponent } from './footer/footer.component';
-=======
 import { BodyComponent } from './body/body.component';
->>>>>>> a6ab48a203cc79d2949910c807a61393b25eea64
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-<<<<<<< HEAD
+    BodyComponent,
     FooterComponent
-=======
-    BodyComponent
->>>>>>> a6ab48a203cc79d2949910c807a61393b25eea64
   ],
   imports: [
     BrowserModule,
